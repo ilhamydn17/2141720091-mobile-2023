0006461c963cf973d73ccaa5d77dbd0a11abfd5b
ilhamydn17 <ilhamyudantyo@gmail.com>
2023-09-11 09:23:20 +0700

    add code materials in week-02

---
+++ week-03/src/main.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  // Part 1
+  // String test = "test2";
+  // if (test == "test1") {
+  //   print("Test1");
+  // } else if (test == "test2") {
+  //   print("Test2");
+  // } else {
+  //   print("Something else");
+  // }
+  // if (test == "test2") print("Test2 again");
+
+  // Part 2
+  String test = "true";
+  if (test) {
+    print("Kebenaran");
+  }
+}
