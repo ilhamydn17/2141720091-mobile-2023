747a84007c1ad136764c6fc54af42d71f6d8e9e3
ilhamydn17 <ilhamyudantyo@gmail.com>
2023-09-11 10:02:18 +0700

    add and fixing week-03

--- week-03/src/main.dart
+++ week-03/src/main.dart
@@ -1,5 +1,6 @@
 void main(List<String> args) {
-  // Part 1
+  // ==> PRAKTIKUM 1
+  // --> Part 1
   // String test = "test2";
   // if (test == "test1") {
   //   print("Test1");
@@ -9,10 +10,40 @@ void main(List<String> args) {
   //   print("Something else");
   // }
   // if (test == "test2") print("Test2 again");
+  // --> Part 2
+  // String test = "true";
+  // if (test) {
+  //   print("Kebenaran");
+  // }
 
-  // Part 2
-  String test = "true";
-  if (test) {
-    print("Kebenaran");
+  // ==> PRAKTIKUM 2
+  // --> Part 1
+  // int counter = 0;
+  // while (counter < 33) {
+  // print(counter);
+  // counter++;
+  // }
+  // --> Part 2
+  // do {
+  //   print(counter);
+  //   counter++;
+  // } while (counter < 77);
+
+  // ==> Praktikum 3
+  // --> Part 1
+  // for (int index = 10; index < 27; index) {
+  //   print(index);
+  // }
+  // --> Part 2
+  // for (int index = 10; index < 27; index++) {
+  //   print(index);
+  // }
+  // --> Part 3
+  for (int index = 10; index < 27; index++) {
+    print(index);
+    if (index == 21) break;
+    else if (index > 1 || index < 7) continue;
+    print(index);
   }
+  
 }
